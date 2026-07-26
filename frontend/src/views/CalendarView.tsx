f9e4ed9d290f97ac75b26af6f0243327e41a29db
Dhananjay40 <dhananjaydeshmukh036@gmail.com>
2026-07-26 19:51:57 +0530

    Fronend fixes for build

--- frontend/src/views/CalendarView.tsx
+++ frontend/src/views/CalendarView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChevronDown, Car, ShoppingBag, Zap, Film, Utensils } from 'lucide-react';
-import { Transaction } from '../types/finance';
+import type { Transaction } from '../types/finance';
 import { TopHeader } from '../components/TopHeader';
 
 interface CalendarViewProps {
